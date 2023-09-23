d31a1eb0b1472ecddd253bb211953dd588fc7e3d
PugBuffy <sergeyselen777@gmail.com>
2023-09-23 16:08:06 +0300

    combine callbacks

--- detector/detector/detector.py
+++ detector/detector/detector.py
@@ -1,8 +1,10 @@
 import rclpy
 import cv2
 import torch
-import numpy 
+import numpy
+import os 
 
+from ament_index_python.packages import get_package_share_directory
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from rclpy.node import Node
@@ -16,53 +18,50 @@ class Detector(Node):
     def __init__(self):
         super().__init__('detector')
         self.subscriber = self.create_subscription(Image, '/image', self.image_callback, 1)
-        self.image = numpy.ndarray
         self.publisher = self.create_publisher(Object2DArray, "/detection", 1)
         self.get_logger().info('Start detector...')    
         
         self.bridge = CvBridge()
         self.device = torch.device("cuda:0")
-        #path_to_engine = "/root/src/detector/yolov8s.engine" # for docker 
-        path_to_engine = "/home/sergey/workspace/ros2_ws/src/detector/yolov8s.engine"
+        path_to_engine = os.path.abspath('src/detector/yolov8s.engine')
         self.engine = TRTModule(path_to_engine, self.device) 
 
-    def image_callback(self, data: Image):
-        self.image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-        self.publish_bbox()
+        print(os.path.join(get_package_share_directory('detector'), 'launch', 'yolov8s.engine'))
 
-    def publish_bbox(self):
-        if type(self.image) == numpy.ndarray:
-            H, W = self.engine.inp_info[0].shape[-2:]
-            self.engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
-  
-            self.image, ratio, dwdh = letterbox(self.image, (W, H))
-            rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-            tensor = blob(rgb, return_seg=False)
-            dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=self.device)
-            tensor = torch.asarray(tensor, device=self.device)
+
+    def image_callback(self, image: Image):
+        image = self.bridge.imgmsg_to_cv2(image, 'bgr8')
+
+        H, W = self.engine.inp_info[0].shape[-2:]
+        self.engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
+        image, ratio, dwdh = letterbox(image, (W, H))
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        tensor = blob(rgb, return_seg=False)
+        dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=self.device)
+        tensor = torch.asarray(tensor, device=self.device)
             
-            data = self.engine(tensor)
+        data = self.engine(tensor)
 
-            bboxes, scores, labels = det_postprocess(data)
-            bboxes -= dwdh
-            bboxes /= ratio
+        bboxes, scores, labels = det_postprocess(data)
+        bboxes -= dwdh
+        bboxes /= ratio
 
-            msg = Object2DArray()
+        msg = Object2DArray()
 
-            for (bbox, score, label) in zip(bboxes, scores, labels):
-                object = Object2D() 
-                bbox = bbox.round().int().tolist()
+        for (bbox, score, label) in zip(bboxes, scores, labels):
+            object = Object2D() 
+            bbox = bbox.round().int().tolist()
 
-                object.x = bbox[0]
-                object.y = bbox[1]
-                object.width = bbox[2] - bbox[0]
-                object.height = bbox[3] - bbox[1]
-                object.score = score.float().tolist()
-                object.label = label.int().tolist()
+            object.x = bbox[0]
+            object.y = bbox[1]
+            object.width = bbox[2] - bbox[0]
+            object.height = bbox[3] - bbox[1]
+            object.score = score.float().tolist()
+            object.label = label.int().tolist()
 
-                msg.objects.append(object)
+            msg.objects.append(object)
 
-            self.publisher.publish(msg)
+        self.publisher.publish(msg)
         
             
 def main(args=None):
