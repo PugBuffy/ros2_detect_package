24ced09845ec1cb26550a79c25ed87f2c65f3ba2
PugBuffy <sergeyselen777@gmail.com>
2023-09-23 16:17:24 +0300

    add vizualizator for debug

---
+++ detector/detector/vizualizator.py
@@ -0,0 +1,59 @@
+import rclpy
+import cv2
+import random
+
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from bboxes_ex_msgs.msg import BoundingBoxes
+from rclpy.node import Node
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+class Vizualizator(Node):
+    def __init__(self):
+        super().__init__('vizualizator')
+        self.bridge = CvBridge()
+        self.classes = ('pedestrian', 'car', 'cyclist', 'truck')
+        self.colors = {cls: [random.randint(0, 255) for _ in range(3)] for cls in self.classes}
+
+        subscribers = [
+            Subscriber(self, Image, '/image'),
+            Subscriber(self, BoundingBoxes, '/bounding_boxes'), 
+            #Subscriber(self, BoundingBoxes, '/debug_detection')
+        ]
+
+        ats = ApproximateTimeSynchronizer(subscribers,
+                                          queue_size=5,
+                                          slop=0.1)
+        ats.registerCallback(self.callback)
+
+        self.publisher = self.create_publisher(Image, '/image_objects', 1)
+        self.get_logger().info('Start vizualizator...')
+
+    def callback(self, img, objects):
+        img = self.bridge.imgmsg_to_cv2(img, 'bgr8')
+        
+        for object in objects.bounding_boxes:
+            cv2.rectangle(img, [object.xmin, object.ymin], [object.xmax, object.ymax],
+                          self.colors[object.class_id], 2)
+            cv2.rectangle(img, [object.xmin, object.ymin - 20], [object.xmax, object.ymin],
+                          self.colors[object.class_id], -1)
+
+            cv2.putText(img, 
+                        f'{object.class_id}:{object.probability:.3f}',
+                        (object.xmin, object.ymin - 2),
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.65, [0, 0, 0],
+                        thickness=2)
+            
+        msg = self.bridge.cv2_to_imgmsg(img, 'bgr8')
+        self.publisher.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    vizualizator = Vizualizator()
+    rclpy.spin(vizualizator)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
