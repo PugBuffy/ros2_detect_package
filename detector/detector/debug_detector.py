83814d1e859f6879dedc31851e70c81afdb00615
PugBuffy <sergeyselen777@gmail.com>
2023-09-23 16:16:32 +0300

    fix problem

--- detector/detector/debug_detector.py
+++ detector/detector/debug_detector.py
@@ -2,11 +2,12 @@ import rclpy
 import cv2
 import torch
 import numpy 
+import os
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from rclpy.node import Node
-from objects_msgs.msg import DebugObject2D, DebugObject2DArray
+from bboxes_ex_msgs.msg import BoundingBox, BoundingBoxes
 from models import TRTModule  # isort:skip
 from models.torch_utils import det_postprocess
 from models.utils import blob, letterbox
@@ -15,61 +16,55 @@ from math import sqrt
 
 class Detector(Node):
     def __init__(self):
-        super().__init__('detector')
+        super().__init__('debug_detector')
         self.subscriber = self.create_subscription(Image, '/image', self.image_callback, 1)
-        self.image = numpy.ndarray
-        self.publisher = self.create_publisher(DebugObject2DArray, "/detection", 1)
-        self.timer = self.create_timer(0, self.publish_bbox)
+        self.publisher = self.create_publisher(BoundingBoxes, "/debug_detection", 1)
         self.get_logger().info('Start detector...')    
         
         self.bridge = CvBridge()
         self.device = torch.device("cuda:0")
-        #path_to_engine = "/root/src/detector/yolov8s.engine" # for docker 
-        path_to_engine = "/home/sergey/workspace/ros2_ws/src/detector/yolov8s.engine"
-        self.engine = TRTModule(path_to_engine, self.device)
+        path_to_engine = os.path.abspath('src/detector/yolov8s.engine')
+        self.engine = TRTModule(path_to_engine, self.device) 
+
         self.classes = ('pedestrian', 'car', 'cyclist', 'truck') 
 
-    def image_callback(self, data: Image):
-        self.image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+    def image_callback(self, image: Image):
+        image = self.bridge.imgmsg_to_cv2(image, 'bgr8')
 
-    def publish_bbox(self):
-        if type(self.image) == numpy.ndarray:
-            H, W = self.engine.inp_info[0].shape[-2:]
-            self.engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
-  
-            self.image, ratio, dwdh = letterbox(self.image, (W, H))
-            rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-            tensor = blob(rgb, return_seg=False)
-            dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=self.device)
-            tensor = torch.asarray(tensor, device=self.device)
+        H, W = self.engine.inp_info[0].shape[-2:]
+        self.engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
+        image, ratio, dwdh = letterbox(image, (W, H))
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        tensor = blob(rgb, return_seg=False)
+        dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=self.device)
+        tensor = torch.asarray(tensor, device=self.device)
             
-            data = self.engine(tensor)
+        data = self.engine(tensor)
 
-            bboxes, scores, labels = det_postprocess(data)
-            bboxes -= dwdh
-            bboxes /= ratio
+        bboxes, scores, labels = det_postprocess(data)
+        bboxes -= dwdh
+        bboxes /= ratio
 
-            msg = DebugObject2DArray()
+        msg = BoundingBoxes()
 
-            for (bbox, score, label) in zip(bboxes, scores, labels):
-                object = DebugObject2D()
-                bbox = bbox.round().int().tolist()
+        for (bbox, score, label) in zip(bboxes, scores, labels):
+            object = BoundingBox()
+            bbox = bbox.round().int().tolist()
 
-                object.probability = score.float().tolist()
-                object.xmin = bbox[0]
-                object.ymin = bbox[1]
-                object.xmax = bbox[2]
-                object.ymax = bbox[3]
-                object.img_height = self.image.shape[0]
-                object.img_width = self.image.shape[1]
-                object.center_dist = int(sqrt((bbox[2] - bbox[0]) ** 2 + (bbox[3] - bbox[1]) ** 2) / 2)
-                object.class_id = self.classes[label.int().tolist()]
-                object.class_id_int = label.int().tolist()
+            object.probability = score.float().tolist()
+            object.xmin = abs(bbox[0])
+            object.ymin = bbox[1]
+            object.xmax = bbox[2]
+            object.ymax = bbox[3]
+            object.img_height = 2160
+            object.img_width = 3840
+            object.center_dist = int(sqrt((bbox[2] - bbox[0]) ** 2 + (bbox[3] - bbox[1]) ** 2) / 2)
+            object.class_id = self.classes[label.int().tolist()]
+            object.class_id_int = label.int().tolist()
 
-                msg.objects.append(object)
+            msg.bounding_boxes.append(object)
 
-            self.publisher.publish(msg)
-            self.get_logger().info('publishing')
+        self.publisher.publish(msg)
         
             
 def main(args=None):
